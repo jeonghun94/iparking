38bdb3a926afd3ca2c57d28ca0162196a73bc8cc
jeonghun94 <jeongh1021@naver.com>
2022-12-19 18:05:14 +0900

    로고 이미지, compnents libs 초기화

---
+++ components/layout.tsx
@@ -0,0 +1,7 @@
+export default function Layout() {
+  return (
+    <div className="w-full h-screen bg-red-200">
+      <h1>Layout2</h1>
+    </div>
+  );
+}
--- pages/t.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <p className="h-full w-full bg-red-500">dsdsdsddddddddd</p>
-    </>
-  );
-}
