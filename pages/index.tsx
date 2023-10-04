ba273b488bfd2a085c76210003a96a24d2d1825d
조정훈 <jeongh1021@naver.com>
2023-10-04 22:58:46 +0900

    스키마 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,24 @@ import Head from "next/head";
 import Link from "next/link";
 import { cls, colors } from "@libs/client/utils";
 import Banner from "@components/banner";
+import { useForm } from "react-hook-form";
+
+interface LoginFormData {
+  email: string;
+  password: string;
+}
 
 export default function Home() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isValid },
+  } = useForm<LoginFormData>();
+
+  const onVaild = (data: LoginFormData) => {
+    console.log(data);
+  };
+
   return (
     <div className="mx-auto flex h-screen w-full max-w-md flex-col items-center justify-center p-3">
       <Head>
@@ -22,36 +38,43 @@ export default function Home() {
         priority
       />
 
-      <input
-        className="w-full rounded-t-md border border-gray-400 px-4 py-3 text-lg"
-        placeholder="아이디"
-      />
-      <input
-        className="w-full rounded-b-md border border-t-0 border-gray-400 px-4 py-3 text-lg"
-        placeholder="비밀번호"
-      />
-      <div className="my-4 flex w-full items-center">
+      <form onSubmit={handleSubmit(onVaild)}>
         <input
-          id="checkbox"
-          type="checkbox"
-          value=""
-          className={`h-4 w-4 accent-${colors.primaryColor} rounded-none border-gray-200 bg-gray-100`}
+          {...register("email", { required: true })}
+          type="email"
+          className="w-full rounded-t-md border border-gray-400 px-4 py-3 text-lg"
+          placeholder="아이디"
         />
-        <label
-          htmlFor="checkbox"
-          className="accent- ml-2 text-lg font-medium text-gray-500"
+        <input
+          {...register("password", { required: true })}
+          type="password"
+          className="w-full rounded-b-md border border-t-0 border-gray-400 px-4 py-3 text-lg"
+          placeholder="비밀번호"
+        />
+        <div className="my-4 flex w-full items-center">
+          <input
+            id="checkbox"
+            type="checkbox"
+            value=""
+            className={`h-4 w-4 accent-${colors.primaryColor} rounded-none border-gray-200 bg-gray-100`}
+          />
+          <label
+            htmlFor="checkbox"
+            className="accent- ml-2 text-lg font-medium text-gray-500"
+          >
+            자동 로그인
+          </label>
+        </div>
+        <button
+          className={cls(
+            `my-5  w-full rounded-md bg-red-500  p-4 text-center text-xl font-semibold text-white hover:bg-red-600
+            ${isValid ? "" : "cursor-not-allowed opacity-50"}
+            `
+          )}
         >
-          자동 로그인
-        </label>
-      </div>
-      <Link
-        className={cls(
-          `my-5  w-full rounded-md bg-red-500  p-4 text-center text-xl font-semibold text-white hover:bg-red-600`
-        )}
-        href={"/search"}
-      >
-        로그인
-      </Link>
+          로그인
+        </button>
+      </form>
       <div className="mt-8 flex flex-col items-center justify-center space-y-3 text-lg">
         <p>
           아이파킹 고객센터{" "}
